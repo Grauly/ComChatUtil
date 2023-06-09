06441c17e3f16486c5fe691695dc052fabb93e28
Grauly <nils.dellemann@web.de>
2023-06-09 17:04:29 +0200

    feat: escape common greetings

--- src/main/java/grauly/comchatutil/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/grauly/comchatutil/mixin/ClientPlayNetworkHandlerMixin.java
@@ -4,17 +4,33 @@ import grauly.comchatutil.ComChatUtil;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
-public class ClientPlayNetworkHandlerMixin {
+public abstract class ClientPlayNetworkHandlerMixin {
+
+    @Shadow public abstract void sendChatCommand(String command);
+
+    @Shadow public abstract void sendChatMessage(String content);
+
+    @Shadow public abstract boolean sendCommand(String command);
 
     @Inject(at = @At("HEAD"), method = "sendChatCommand")
-    public void sendChatCommand(String command, CallbackInfo ci) {
+    public void changeStateOnCommand(String command, CallbackInfo ci) {
         if(command.equalsIgnoreCase("cc") || command.equalsIgnoreCase("communitychat")) {
             ComChatUtil.inComChat = !ComChatUtil.inComChat;
+        }
+    }
+
+    @Inject(at = @At("HEAD"), method = "sendChatMessage")
+    public void escapeGreetings(String content, CallbackInfo ci) {
+        if(ComChatUtil.inComChat && content.matches("[wW][bB].?|[wW]elcome.?")) {
+            sendChatCommand("cc");
+            sendChatMessage(content);
+            sendCommand("cc");
         }
     }
 
